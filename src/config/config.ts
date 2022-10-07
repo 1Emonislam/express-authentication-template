d0205755f22a86f828a1f7df5948a0ad2466c0f6
1emonislam <emonislamweb@gmail.com>
2022-10-07 16:26:42 +0600

    api version

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 export const config = {
     db_uri: process.env.DB_URI || '',
     port: parseInt(process.env.PORT || '5000'),
-    api_version: process.env.API_VERSION || '/api/v1',
+    api_version: process.env.API_VERSION || 'v1',
     jwt_secret: process.env.JWT_SECRET || '',
     cookie_expires: process.env.COOKIE_EXPIRES || '30d',
     cookie_short_expires: process.env.COOKIE_SHORT_EXPIRES || '1h'
