053f8761ff28b34d2899a0b5f45e3a0792d9e3e4
1emonislam <emonislamweb@gmail.com>
2022-10-07 23:58:54 +0600

    websocket connection

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,31 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express, { Response, Request } from "express";
+import http from 'http'
+import { Server } from 'socket.io'
 import morgan from 'morgan'
 import mongoose from "mongoose";
+import cors from 'cors'
 const { errorLog, errorHandlerNotify } = require('express-error-handle')
 import { config } from "./config/config";
 import authRoutes from './api/routes/auth.routes'
 //middlewares
 const app = express();
+const serverApp = http.createServer(app);
+const io = new Server(serverApp, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+//middlewares
+app.use(cors({
+  origin: config.cors_support,
+  credentials: true
+}));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
 app.use(morgan('dev'))
 //database connected
 mongoose
@@ -22,6 +39,16 @@ mongoose
   .catch((err: any) => {
     console.error(`⛔⛔⛔ Database connections Failed ⛔⛔⛔: ${JSON.stringify(err)}`);
   });
+
+//socket
+io.on('connection', (socket: any) => {
+  app.set('socket', socket)
+  console.log('user connected ws', socket.id);
+  socket.emit('status', 'Hello from websocket realtime connencted');
+  socket.on('disconnect', () => {
+    console.log('client disconnected');
+  })
+});
 //all routes here
 app.use(`/api/${config.api_version}/auth`, authRoutes)
 app.get('/favicon.ico', (_req: Request, res: Response) => {
--- src/config/config.ts
+++ src/config/config.ts
@@ -2,14 +2,16 @@ export const config = {
     db_uri: process.env.DB_URI || '',
     port: parseInt(process.env.PORT || '5000'),
     api_version: process.env.API_VERSION || 'v1',
+    cors_support: process.env.CORS_SUPPORT || "*",
     jwt_secret: process.env.JWT_SECRET || '',
     cookie_expires: process.env.COOKIE_EXPIRES || '30d',
     cookie_short_expires: process.env.COOKIE_SHORT_EXPIRES || '1h',
     client_url: process.env.CLIENT_URL || '',
-    logo:process.env.LOGO,
-    domain:process.env.DOMAIN,
-    gmail_host:process.env.GMAIL_HOST,
-    admin_sender_email:process.env.ADMIN_SENDER_EMAIL,
-    gmail_password:process.env.GMAIL_PASSWORD,
+    logo: process.env.LOGO,
+    domain: process.env.DOMAIN,
+    gmail_host: process.env.GMAIL_HOST,
+    admin_sender_email: process.env.ADMIN_SENDER_EMAIL,
+    gmail_password: process.env.GMAIL_PASSWORD,
+
 
 }
